0b40d4514c3a5687962dc6d79b6ab148aa07cd51
Edwin Chajon <junchajo@gmail.com>
2020-07-10 12:47:57 -0700

    ex.4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,28 @@
-def has_negatives(a):
+def has_negatives(array):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    number_list = {}
+    
+    #loop through the array and hash
+    for number in array:
+        # if there is an absolute value
+        if number_list.get(abs(number)) is not None:
+            #add to the number_list
+            number_list[abs(number)] += 1
+        else:
+            #otherwise if no absolute value, give value of 1
+            number_list[abs(number)] = 1
+    #filter through number_list
+    for number in number_list:
+        #if value greater than one 
+        if number_list[number] > 1:
+            #add to the result
+            result.append(number)
 
     return result
+
 
 
 if __name__ == "__main__":
